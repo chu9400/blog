eddd363fa0255a968ae8faeb444c296f4acc907c
Hanul <chu9400@naver.com>
2022-07-20 22:48:25 +0900

    test

---
+++ README.md
@@ -0,0 +1,12 @@
+### JSX 문법
+1. className
+
+<div class="APP"></div> x
+<div className="APP"></div> 0
+
+2. 변수 꽃을 땐 중괄호 문법 {변수명}
+let post = 'test';   
+<h3 id={post}>{post}</h3>   
+
+3. style 넣을 때
+ style={{이름:'값', 이름2:'값2'}}
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import{useState} from 'react';
+
+function App() {
+  let posts = '교대 우동 맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬독학']);
+  let [따봉, 따봉변경] = useState('안녕');
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>ReactBlog</div>
+      </div>
+      <div className="list">
+        <h4>{글제목[0]} 
+          <button onClick={ () => {
+            글제목[0] = '여자 코트 추천';
+            글제목변경(글제목);
+          }} >
+            버튼
+          </button>
+          <span onClick={ () => 따봉변경('반가워')}>👁</span>
+          {따봉}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
